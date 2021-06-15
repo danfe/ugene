b50c587f3e9c6c50b35a9629f6b5af2d971c5412
ichebyki <ichebykin@unipro.ru>
2021-06-15 17:25:54 +0700

    Add variant for intall path. Use var UGENE_BUILD_RELEASE_INSTALL_GLOBAL_PATH

--- installer/macosx/productbuild.sh
+++ installer/macosx/productbuild.sh
@@ -16,36 +16,38 @@ if [[ -z "$3" ]]; then
     exit -2
 fi
 
-#productbuild \
-#    --component "$1" \
-#    /Applications/ \
-#    "$2"
+if [ -n "$UGENE_BUILD_RELEASE_INSTALL_GLOBAL_PATH" ]; then
+    productbuild \
+        --component "$1" \
+        /Applications/ \
+        "$2"
+else
+    pkgbuild \
+        --analyze \
+        --root "$1" \
+        "Unipro UGENE-component.plist"
 
-pkgbuild \
-    --analyze \
-    --root "$1" \
-    "Unipro UGENE-component.plist"
+    plutil \
+        -replace BundleIsRelocatable \
+        -bool NO \
+        "Unipro UGENE-component.plist"
 
-plutil \
-    -replace BundleIsRelocatable \
-    -bool NO \
-    "Unipro UGENE-component.plist"
+    pkgbuild \
+        --root "$1" \
+        --identifier net.ugene.ugene \
+        --version 39.0 \
+        --install-location Applications/UGENE.app \
+        --component-plist "Unipro UGENE-component.plist" \
+        "Package.pkg"
 
-pkgbuild \
-    --root "$1" \
-    --identifier net.ugene.ugene \
-    --version 39.0 \
-    --install-location Applications/UGENE.app \
-    --component-plist "Unipro UGENE-component.plist" \
-    "Package.pkg"
+    productbuild --synthesize --package "Package.pkg" Distribution.xml
 
-productbuild --synthesize --package "Package.pkg" Distribution.xml
+    cat Distribution.xml \
+        | perl -n -e 'if (/.*\<installer-gui-script.*/) {print $_; print "    <domains enable_anywhere=\"false\" enable_currentUserHome=\"true\" enable_localSystem=\"false\"/>\n";}else {print $_;}' \
+    > Distribution2.xml
 
-cat Distribution.xml \
-    | perl -n -e 'if (/.*\<installer-gui-script.*/) {print $_; print "    <domains enable_anywhere=\"false\" enable_currentUserHome=\"true\" enable_localSystem=\"false\"/>\n";}else {print $_;}' \
-> Distribution2.xml
-
-productbuild --distribution ./Distribution2.xml --package-path "Package.pkg" "$2"
+    productbuild --distribution ./Distribution2.xml --package-path "Package.pkg" "$2"
+fi
 
 productsign \
     --sign "Developer ID Installer: Alteametasoft" \
