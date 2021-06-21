721a8ef3cb1e01f3554eab42395e200d17ac64be
ichebyki <ichebykin@unipro.ru>
2021-06-21 12:02:40 +0700

    macos: add special code signing for java8, python2

--- installer/macosx/codesign.mac.sh
+++ installer/macosx/codesign.mac.sh
@@ -63,8 +63,8 @@ find "$contents_dir"/PlugIns -type f \
     "{}" \; \
 || exit -1
 
-echo "============= Sign all files in $contents_dir/Resources dir, except java8 ============="
-find "$contents_dir"/Resources -not \( -path "$contents_dir"/Resources/tools/java8 -prune \) -type f \
+echo "============= Sign all files in $contents_dir/Resources dir, except java8,python2 ============="
+find "$contents_dir"/Resources -not \( -path "$contents_dir"/Resources/tools/java8 -prune -o -path "$contents_dir"/Resources/tools/python2 -prune \) -type f \
 -exec codesign \
     --sign "Developer ID Application: Alteametasoft" \
     --timestamp \
@@ -85,7 +85,20 @@ find "$contents_dir"/Resources/tools/java8 -type f \
     --verbose=11 \
     $CODE_SIGN_OPTIONS \
     --strict \
-    ${entitlements} \
+    --entitlements entitlements-java.plist \
+    "{}" \; \
+|| exit -1
+
+echo "============= Sign all files in $contents_dir/Resources/tools/python2 dir ============="
+find "$contents_dir"/Resources/tools/python2 -type f \
+-exec codesign \
+    --sign "Developer ID Application: Alteametasoft" \
+    --timestamp \
+    --force \
+    --verbose=11 \
+    $CODE_SIGN_OPTIONS \
+    --strict \
+    --entitlements entitlements-python2.plist \
     "{}" \; \
 || exit -1
 
